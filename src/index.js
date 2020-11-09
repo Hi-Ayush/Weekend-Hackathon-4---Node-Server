75fdd796f92d3627b52735709592d6ccafbd4bfc
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-11-09 17:29:19 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if (req.url === "/welcome") {
-    res.writeHead(200,{'Content-Type':'text/plain'})
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Welcome to Dominos!");
     res.end();
-  }
- else if (req.url === "/contact") {
-    res.writeHead(200,{'Content-Type':'application/json'})
-    res.write(
-      JSON.stringify({
-        phone: "18602100000",
-        email: "guestcaredominos@jublfood.com"
-      })
-    );
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write({
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    });
+
     res.end();
-  }
-  else{
-    res.writeHead(400,{'Content-Type':'text/plain'})
+  } else {
+    res.writeHead(400, { "Content-Type": "text/plain" });
     res.end();
   }
 }
